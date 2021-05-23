f26f77d4b4987efbcfd9b8d52d3da4f63481d220
Marcos Vidovix <m.vidovix.s@gmail.com>
2021-05-22 23:06:14 -0300

    Commit inicial
    código desenvolvido durante curso da COD3R disponível no link: https://www.udemy.com/share/101qH0BEAacFlVQno=/

---
+++ curso-php/basico/ola.php
@@ -0,0 +1,14 @@
+<div class="titulo">Olá PHP</div>
+<h2> Exemplo A</h2>
+<?php
+    echo('Olá ');
+    echo('Mundo!');
+?>
+<h2> Exemplo B</h2>
+<?= "<br> Outra forma de exibir informação da tela sem utilizar o echo" ?>
+
+<h2>Exemplo C</h2>
+
+    <?php
+        phpinfo();
+    ?>
