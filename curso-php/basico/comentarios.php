e18750c7edd6679a1e7a45158642d33e2ae4ee8e
Marcos Vidovix <m.vidovix.s@gmail.com>
2021-05-22 23:57:33 -0300

    1º Capitulo do curso.
    Código desenvolvido durante curso da COD3R disponível no link: https://www.udemy.com/share/101qH0BEAacFlVQno=/

---
+++ curso-php/basico/comentarios.php
@@ -0,0 +1,18 @@
+<div class="titulo">Comentários PHP</div>
+
+<?php
+echo('Estou no PHP');
+//echo "Comentário";
+#echo "Comentário";
+?>
+
+<p> Depois do primeiro bloco!</p>
+
+<?php
+/* linha do comentário
+segunda linha do comentário
+terceira linha do comentário
+*/
+?>
+
+<p> Depois do segundo bloco!</p>
--- curso-php/index.php
+++ curso-php/index.php
@@ -16,10 +16,22 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01</h3>
+                    <h3>Módulo 01 - Básico</h3>
                     <ul>
                         <li>
                             <a href="exercicio.php?dir=basico&file=ola">Olá PHP</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=html">Integração HTML</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=CSS">Integração CSS</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=desafio">Desafio</a>
                         </li>
                     </ul>
                 </div>
